bf895a708c11e22e1abcc8b62bbcaf77fe4df725
ramenha0141 <ramenhaoishii@gmail.com>
2022-01-31 16:46:27 +0900

    update

--- transpile.js
+++ transpile.js
@@ -1,14 +1,33 @@
+#!/usr/bin/env node
 import fs from 'fs';
 import antlr4 from 'antlr4';
 import JavaScriptLexer from './JavaScriptLexer.js';
 import JavaScriptParser from './JavaScriptParser.js';
 import JavaScriptParserVisitor from './JavaScriptParserVisitor.js';
-const input = fs.readFileSync('input.js', 'utf-8');
-const chars = new antlr4.InputStream(input);
-const lexer = new JavaScriptLexer(chars);
-const tokens = new antlr4.CommonTokenStream(lexer);
-const parser = new JavaScriptParser(tokens);
-parser.buildParseTrees = true;
-const tree = parser.program();
-const visitor = new JavaScriptParserVisitor();
-fs.writeFileSync('output.js', visitor.visit(tree));
+function compile(option) {
+    const input = fs.readFileSync(option.filename, 'utf-8');
+    const chars = new antlr4.InputStream(input);
+    const lexer = new JavaScriptLexer(chars);
+    const tokens = new antlr4.CommonTokenStream(lexer);
+    const parser = new JavaScriptParser(tokens);
+    parser.buildParseTrees = true;
+    const tree = parser.program();
+    const visitor = new JavaScriptParserVisitor();
+    fs.writeFileSync(option.outputfilename || (option.filename + '.js'), visitor.visit(tree));
+}
+const args = process.argv.slice(2);
+const option = {};
+for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    switch (arg) {
+        case '-o': {
+            i++;
+            option.outputfilename = args[i];
+            break;
+        }
+        default: {
+            option.filename = arg;
+        }
+    }
+}
+if (option.filename) compile(option);
